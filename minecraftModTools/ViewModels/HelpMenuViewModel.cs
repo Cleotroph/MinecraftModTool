50da9c9f4338a2abda342d0bc65bde4188385273
rhit-cerasoml <cerasoml@rose-hulman.edu>
2023-08-14 21:10:52 -0400

    implementation of MVVM design and help menu github link

---
+++ minecraftModTools/ViewModels/HelpMenuViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace minecraftModTools.ViewModels
+{
+    class HelpMenuViewModel : INotifyPropertyChanged
+    {
+
+        public ICommand Github { get; init; }
+
+        public HelpMenuViewModel() {
+            Github = new RelayCommand(() => {
+                Process.Start(new ProcessStartInfo("https://github.com/Cleotroph/MinecraftModTool") { UseShellExecute = true });
+            });
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
